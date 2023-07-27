e83c50e04de26622e5154afa3123174defd3c439
Anton Polkanov <ap.polkanov@gmail.com>
2023-07-27 16:44:26 +1000

    initial

---
+++ MAUI.Bug.FFImageLoading/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUI.Bug.FFImageLoading;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MAUI.Bug.FFImageLoading/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI.Bug.FFImageLoading;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
